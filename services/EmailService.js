9521c4f1cca4b8bb12d4718ff41dbc8c686b38a9
juanca1620 <149030775+juanca1620@users.noreply.github.com>
2025-04-26 14:33:19 -0500

    Update EmailService.js

--- services/EmailService.js
+++ services/EmailService.js
@@ -69,7 +69,7 @@ const defaultHtmlTemplate = `
       <p>
         Desde hoy, tenés acceso a una experiencia pensada para conectar, compartir y crear valor real.
       </p>
-      <a href="https://colab.com/inicio" class="btn">Ir a la plataforma</a>
+      <a href="https://nnndn11r-5173.usw3.devtunnels.ms/login" class="btn">Ir a la plataforma</a>
       <div class="footer">
         © 2025 Colab. Todos los derechos reservados.
       </div>
